4eb81155aadd69d86cfe77afee360cde17218acb
AjdaAkter <ajdaakter87@gmail.com>
2019-04-27 16:22:56 +0300

    Todos,Login,Register

--- src/components/Register/index.js
+++ src/components/Register/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { withRouter } from 'react-router-dom';
+
+import { Link, withRouter } from 'react-router-dom';
 import './styles.css';
 const Register = (props) => {
   const [email, setEmail] = useState('');
@@ -14,7 +15,8 @@ const Register = (props) => {
     }
   }
   const handleOnSubmit = (event) => {
-    // Reset our error every time the form is submitted
+    console.log('SUBMIT');
+        // Reset our error every time the form is submitted
     setError(null);
     setLoading(true);
     fetch(
@@ -28,35 +30,50 @@ const Register = (props) => {
           email: email,
           password: password
         })
-    })
-    .then((response) => {
-      const json = response.json();
-      if (response.ok) {
-        return json;
-      }
-      
-      return json.then((data) => Promise.reject(data));
-    })
-    .then((json) => {
-      setLoading(false);
-      localStorage.setItem('token', json.token);
-      props.history.push('/todos');
-    })
+    }).then((response) => {
+        console.log('SUCCESS');
+        console.log(response);
+        const json = response.json();
+        if (response.ok) {
+          return json;
+        }
+        
+        return json.then((data) => Promise.reject(data));
+        // return response.json();
+      })
+      .then((json) => {
+        console.log('JSON');
+        console.log(json);
+        setLoading(false);
+        localStorage.setItem('token', json.token);
+        props.history.push('/todos');
+      })
+    //   .then((response) => {
+    //   console.log('SUCCESS');
+    //   console.log(response);
+    //   })
     .catch((error) => {
       setLoading(false);
+      console.log('ERROR');
+      console.log(error);
+
       setError(error.error);
+      setLoading(false);
     });
     event.preventDefault(); // to prevent the form from submitting
   }
-  return (
+return (
     <div className="App">
-      <h1>Register</h1>
-      <form onSubmit={handleOnSubmit}>
+      <h1 className="Register-header">Register</h1>
+      <form className="Register-form" onSubmit={handleOnSubmit}>
         <input disabled={loading} type="email" onChange={(event) => handleOnChange(event, 'email')} required="required" value={email} placeholder="Email address" />
-        <input disabled={loading} type="password" onChange={(event) => handleOnChange(event, 'password')} required="required" value={password} placeholder="Password" />
+        <input disabled={loading} type="password" onChange={(event) => handleOnChange(event, 'password')} required="required" value={password} placeholder="Password" /><br></br>
+        
         {error && <p style={{ color: 'red' }}>{error}</p>}
+         
         <button disabled={loading} type="submit">Submit</button>
       </form>
+      <Link to="/login">Login</Link>
     </div>
   );
 };
